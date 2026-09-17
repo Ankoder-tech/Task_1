ba7fb928ea80152c5ec30f4713575bcf38c7361e
Ankoder-tech <beingankit888@gmail.com>
2026-09-17 10:08:29 +0530

    feat:add Register_screen

--- app/src/main/java/com/example/myappui/RegisterActivity.kt
+++ app/src/main/java/com/example/myappui/RegisterActivity.kt
@@ -1,31 +1,20 @@
 package com.example.myappui
-import android.content.Intent
+
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.registeractivity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val loginButton=findViewById<Button>(R.id.loginButton)
-        loginButton.setOnClickListener{
-            val intent=Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        val registerButton=findViewById<Button>(R.id.registerButton)
-        registerButton.setOnClickListener{
-            val intent=Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
