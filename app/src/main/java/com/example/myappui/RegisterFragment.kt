de18be1ed14f3c4a423841fc4b4484b8a81995ef
Ankoder-tech <beingankit888@gmail.com>
2026-09-17 17:52:44 +0530

    feat:Adjust images

--- app/src/main/java/com/example/myappui/RegisterFragment.kt
+++ app/src/main/java/com/example/myappui/RegisterFragment.kt
@@ -1,6 +1,27 @@
 package com.example.myappui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class RegisterFragment : Fragment(R.layout.fragment_register)
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val alreadyAccount =
+            view.findViewById<TextView>(R.id.already_account)
+
+        alreadyAccount.setOnClickListener {
+
+            findNavController().navigate(
+                R.id.action_register_to_login
+            )
+        }
+    }
+}
